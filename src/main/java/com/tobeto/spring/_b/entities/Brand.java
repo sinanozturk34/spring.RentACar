1d4eb37064cb6f0c1050395d1d121cb009881308
Sinan Ozturk <sinansinan_@outlook.com.tr>
2025-08-14 19:17:38 +0300

    foreign key

---
+++ src/main/java/com/tobeto/spring/_b/entities/Brand.java
@@ -0,0 +1,22 @@
+package com.tobeto.spring._b.entities;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Table(name = "brands")
+@Entity
+public class Brand {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "brand")
+   private List<Car> cars;
+
+}
---
+++ src/main/java/com/tobeto/spring/_b/entities/Car.java
@@ -0,0 +1,24 @@
+package com.tobeto.spring._b.entities;
+
+import jakarta.persistence.*;
+
+@Table(name="cars")
+@Entity
+public class Car
+{
+  @Id
+  @Column(name="id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @Column(name="model_year")
+  private int modelYear;
+
+  @Column(name="model_name")
+  private String modelName;
+
+  @ManyToOne
+  @JoinColumn(name = "brand_id")
+  private Brand brand;
+
+}
