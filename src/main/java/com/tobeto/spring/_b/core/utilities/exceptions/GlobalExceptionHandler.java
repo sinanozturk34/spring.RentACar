7999c84a829a86e3417649c11a896123bc3a18e5
Sinan Ozturk <sinansinan_@outlook.com.tr>
2025-12-18 20:40:53 +0300

    global exception handler

--- src/main/java/com/tobeto/spring/_b/core/utilities/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/tobeto/spring/_b/core/utilities/exceptions/GlobalExceptionHandler.java
@@ -1,18 +1,46 @@
 package com.tobeto.spring._b.core.utilities.exceptions;
+import org.slf4j.event.KeyValuePair;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     // type'ını vermek
-    @ExceptionHandler({ MethodArgumentNotValidException.class })
+    @ExceptionHandler({MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleValidationException() {
-        return "Validasyon hatası";
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : exception.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return errors;
+    }
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleRuntimeError(RuntimeException exception){
+        return exception.getMessage();
+    }
+//Know Types
+//Unknown  Types
+//------------------------------
+    @ExceptionHandler({Exception.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleUnknownErrors() {
+        return "Bilinmedik Hata";
     }
 }
+
+
+
+
+
